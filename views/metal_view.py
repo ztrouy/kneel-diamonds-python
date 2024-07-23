f1fcd86cb85134aa41ce3fa8b1e6571e921dcdb5
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-07-22 22:24:06 -0500

    Added logic to update_metal method to update the price of a Metal

--- views/metal_view.py
+++ views/metal_view.py
@@ -10,8 +10,18 @@ def get_single_metals(url):
 def create_metal(metal_data):
     pass
 
-def update_metal(metal_data):
-    pass
+def update_metal(id, metal_data):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+            UPDATE Metals
+                SET price = ?
+            WHERE id = ?
+        """, (metal_data["price"], id))
+        rows_affected = db_cursor.rowcount
+
+    return True if rows_affected > 0 else False
 
 def delete_metal(pk):
     pass
