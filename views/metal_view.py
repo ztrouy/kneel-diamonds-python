cf64a8be4c51bfe53367c98b2e771dcfc7d561ca
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-07-22 22:03:36 -0500

    Added placeholder methods for basic CRUD methods

---
+++ views/metal_view.py
@@ -0,0 +1,17 @@
+import sqlite3
+import json
+
+def get_metals(url):
+    pass
+
+def get_single_metals(url):
+    pass
+
+def create_metal(metal_data):
+    pass
+
+def update_metal(metal_data):
+    pass
+
+def delete_metal(pk):
+    pass
